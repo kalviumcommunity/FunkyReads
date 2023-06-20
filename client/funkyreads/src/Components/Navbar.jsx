b55ba5affbda288454ab1cda64eb42f680930bf2
Swati Mohanty <swatisucharitamohanty1@gmail.com>
2023-06-20 14:54:49 +0530

    changes created

--- client/funkyreads/src/Components/Navbar.jsx
+++ client/funkyreads/src/Components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import {Box} from "@chakra-ui/react"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Box border={"3px"} bgColor={"red"} width={"100%"} height={"100px"}>
+
+    </Box>
   )
 }
 
---
+++ client/funkyreads/src/Theme/Theme.js
@@ -0,0 +1,5 @@
+import { extendTheme } from '@chakra-ui/react'
+import  {} from "./textStyles";
+
+
+export const Theme = extendTheme({})
