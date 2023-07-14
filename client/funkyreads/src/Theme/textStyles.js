53b8e20a34418f072d1819bce9e466b56d4b3419
Swati Sucharita Mohanty <103181682+swati082001@users.noreply.github.com>
2023-07-14 16:40:18 +0530

    Revert "Completed Single ProductPage"

--- client/funkyreads/src/Theme/textStyles.js
+++ client/funkyreads/src/Theme/textStyles.js
@@ -40,19 +40,6 @@ export const textStyles = {
         fontSize:"20px",
         color:"#9F1111",
         marginTop:"-10px"
-    },
-    singlePagehead:{
-        fontSize:"30px",
-        fontWeight:"700",
-    },
-    singlepageText:{
-        fontSize:"20px",
-        fontWeight:"600",
-        letterSpacing:"1px"
-    },
-    TBR:{
-        fontSize:"24px",
-        fontWeight:"700",
     }
 
 
